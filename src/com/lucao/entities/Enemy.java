101c0242246961972cf9c9bc86fb64a2d0b5302d
GrandeLucao <lucasgameronbrazil@gmail.com>
2023-06-14 00:29:44 -0300

    metade feito

--- src/com/lucao/entities/Enemy.java
+++ src/com/lucao/entities/Enemy.java
@@ -2,10 +2,30 @@ package com.lucao.entities;
 
 import java.awt.image.BufferedImage;
 
+import com.lucao.main.Game;
+import com.lucao.world.World;
+
 public class Enemy extends Entity{
+	
+	private double speed=1;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
+	}
+	
+	public void tick() {
+		if((int)x<Game.player.GetX() && World.isFree((int)(x+speed), this.GetY())) {
+			x+=speed;
+		}
+		else if((int)x>Game.player.GetX() && World.isFree((int)(x-speed), this.GetY())) {
+			x-=speed;
+		}
+		else if((int)y<Game.player.GetY() && World.isFree(this.GetX(), (int)(y+speed))) {
+			y+=speed;
+		}
+		else if((int)y>Game.player.GetY() && World.isFree(this.GetX(), (int)(y-speed))) {
+			y-=speed;
+		}
 	}
 
 }
